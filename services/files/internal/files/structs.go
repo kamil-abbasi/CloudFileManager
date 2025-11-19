ee2a4bd6e1f00b3755acc5a65b109a221367be52
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-11-19 15:53:14 +0100

    added create and update dtos

--- services/files/internal/files/structs.go
+++ services/files/internal/files/structs.go
@@ -6,3 +6,18 @@ type File struct {
 	Location string
 	Size     uint64
 }
+
+type FileCreateDto struct {
+	Id       string
+	Filename string
+	Location string
+	Size     uint64
+}
+
+type FileUpdateDto struct {
+	Id     string
+	Fields struct {
+		Filename string
+		Location string
+	}
+}
