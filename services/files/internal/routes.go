55a9257534e5f85f0508d33aade4b3ab67a75a10
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-11-19 15:50:46 +0100

    added GET /healthcheck endpoint

--- services/files/internal/routes.go
+++ services/files/internal/routes.go
@@ -4,12 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 
 	v1 "github.com/kamil-abbasi/CloudFileOperationsBackend/internal/api/v1"
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
 )
 
-func NewApiRouter() *gin.Engine {
+func NewApiRouter(config *config.Config) *gin.Engine {
 	r := gin.Default()
 
-	v1.NewRouter(r)
+	r.GET("/healthcheck")
+
+	v1.NewRouter(r, config)
 
 	return r
 }
