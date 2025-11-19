fe7b42f372db726bb0910c7e8e046d9177551332
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-11-19 15:52:36 +0100

    added config param to NewRouter function and fixed POST endpoint path

--- services/files/internal/api/v1/router.go
+++ services/files/internal/api/v1/router.go
@@ -3,15 +3,16 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
 	files "github.com/kamil-abbasi/CloudFileOperationsBackend/internal/files/controllers/v1"
 )
 
-func NewRouter(r *gin.Engine) {
+func NewRouter(r *gin.Engine, config *config.Config) {
 	v1Router := r.Group("/v1")
 	{
 		filesRouter := v1Router.Group("/files")
 		{
-			controller := files.NewController()
+			controller := files.NewController(config)
 
 			filesRouter.GET("/:id", controller.FindOne)
 
