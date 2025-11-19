b695e1137d7337befae5aa226e0136df2d2f971f
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-11-19 15:48:58 +0100

    moved run logic into Run function, added database and config

--- services/files/cmd/server/main.go
+++ services/files/cmd/server/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal"
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/config"
+	"github.com/kamil-abbasi/CloudFileOperationsBackend/internal/database"
 )
 
 func main() {
-	r := internal.NewApiRouter()
+	if err := Run(); err != nil {
+		log.Fatalf("Failed to start app, details: %v", err)
+	}
+}
 
+func Run() error {
+	cfg := config.New()
+	cfg.Load()
+
+	os.MkdirAll(cfg.RootPath, 0755)
+
+	db := database.New(cfg)
+	db.Init()
+
+	r := internal.NewApiRouter(cfg)
 	r.SetTrustedProxies([]string{"reverse-proxy"})
-
-	r.GET("/healthcheck")
-
 	r.Run()
+
+	return nil
 }
